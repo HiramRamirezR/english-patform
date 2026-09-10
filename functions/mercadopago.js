21cc0c0d118cf6774ce7bb5f89244f46a693aee1
Hiram Ramírez <68158101+HiramRamirezR@users.noreply.github.com>
2026-09-10 14:13:52 -0600

    test email

--- functions/mercadopago.js
+++ functions/mercadopago.js
@@ -164,7 +164,7 @@ exports.handler = async function (event, context) {
             const preference = {
                 reason: 'Suscripcion Mensual Moonsforest',
                 external_reference: userId,
-                payer_email: email,
+                payer_email: 'test_buyer@fakeemail.com',
                 auto_recurring: {
                     frequency: 1,
                     frequency_type: 'months',
